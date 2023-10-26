63700b0447e69f7c3edb053e20fd5100288e9569
Mamun <mamun.anjum@damago.eu>
2023-10-26 11:08:33 +0200

    update

---
+++ wow/src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.Array;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void getLinkId(){
+        //new list
+        List<String> list = new Array<String>();
+
+//hello!
+        System.out.println();
+    }
+}
